6e48c1fd5c56db4c0d80d12873eb88afaf02a63f
noman5945 <abdullahnoman.5945@gmail.com>
2024-09-09 19:37:09 +0600

    Deployed

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDX-KpsR1Gh-UTQyD3XEAE9D2C41yFVHNo",
+  authDomain: "car-service-83050.firebaseapp.com",
+  projectId: "car-service-83050",
+  storageBucket: "car-service-83050.appspot.com",
+  messagingSenderId: "9797594128",
+  appId: "1:9797594128:web:a8677c905a0022a1e62e95",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
